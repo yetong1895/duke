6887803740592c3e85e3cbed5918929387a17256
yetong <e0310555@u.nus.edu>
2019-08-22 14:33:30 +0800

    level-1: Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        echo echoObject = new echo();
+        Scanner inputs = new Scanner(System.in);
+        while(true) {
+            String userInputs;
+            userInputs = inputs.nextLine();
+            if(userInputs.equals("bye")) {
+                echoObject.exitMessage();
+                break;
+            } else {
+                echoObject.repeatInput(userInputs);
+            }
+        }
     }
 }
---
+++ src/main/java/echo.java
@@ -0,0 +1,11 @@
+public class echo {
+    public echo() {
+        System.out.println("Hello! I'm Duke");
+    }
+    public void repeatInput(String input) {
+        System.out.printf("%s\n",input);
+    }
+    public void exitMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
